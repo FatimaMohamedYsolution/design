64e651a35273093002537412eaaf07c3ee00e383
FatimaMohamedYsolution <157613125+FatimaMohamedYsolution@users.noreply.github.com>
2024-11-03 16:18:38 +0200

    style page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,70 @@
 
-var swiper = new Swiper("#mySwiper", {
-    autoplay: {
-      delay: 4000,
-    },
-    slidesPerView: 1,
-    spaceBetween: 60,
-    slidesPerGroup: 1,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    breakpoints: {
-          0: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          576: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          992: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          1200: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          1400: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-        },
-  });
+
+// window.addEventListener("scroll" ,function(){
+//   if (window.scrollY > 300) {
+// $('.heading.bottom-heading.navbar').css({ backgroundColor:'red'});
+//   } 
+//   // else {
+//   //   // $('#naaaaaav').css({top:'-12px'});
+//   // }
+
+//   console.log(window.scrollY)
+// },false)
+
+var headingNavbar=document.querySelectorAll(".navbar");
+
+window.addEventListener("scroll" ,() =>{
+  if (window.scrollY > 300) {
+    headingNavbar.classList.add("scrolled");
+  } else {
+    headingNavbar.classList.remove("scrolled");
+  }
+
+  console.log(window.scrollY)
+})
+
+
+
+
+
+// var swiper = new Swiper("#mySwiper", {
+//     autoplay: {
+//       delay: 4000,
+//     },
+//     slidesPerView: 1,
+//     spaceBetween: 60,
+//     slidesPerGroup: 1,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//     breakpoints: {
+//           0: {
+//             slidesPerView: 1,
+//             spaceBetween: 20,
+//           },
+//           576: {
+//             slidesPerView: 1,
+//             spaceBetween: 20,
+//           },
+//           768: {
+//             slidesPerView: 1,
+//             spaceBetween: 20,
+//           },
+//           992: {
+//             slidesPerView: 1,
+//             spaceBetween: 20,
+//           },
+//           1200: {
+//             slidesPerView: 2,
+//             spaceBetween: 20,
+//           },
+//           1400: {
+//             slidesPerView: 2,
+//             spaceBetween: 10,
+//           },
+//         },
+//   });
 
 
 // 
@@ -49,6 +77,43 @@ var swiper = new Swiper("#mySwiper", {
 // });
 
 
+
+var swiper = new Swiper("#mySwiper", {
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false, // Continue autoplay even after user interactions
+
+  },
+  centeredSlides: true,
+  slidesPerGroup: 1,
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        576: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        992: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+       
+      },
+  });
 
 
 
